fb71f4fd4db0817f161e0e789c0200a108cdec98
unknown <bravindraseo@gmail.com>
2025-08-29 23:12:52 +0530

    updated email authentication
    updated email authentication

--- workflows/parent_dag.py
+++ workflows/parent_dag.py
@@ -13,7 +13,7 @@ ARGS = {
     "depends_on_past": False,
     "email_on_failure": False,
     "email_on_retry": False,
-    "email": ["bravindraseo@gmail.com"],
+    "email": ["***@gmail.com"],
     "email_on_success": False,
     "retries": 1,
     "retry_delay": timedelta(minutes=5)
